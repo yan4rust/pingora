1305294c84fd06b6177886a108625062300a9bd0
evanyuan <evanyuan163@163.com>
2024-11-08 21:00:24 +0800

    add args to control proxy

--- pingora-proxy/examples/h2c_proxy.rs
+++ pingora-proxy/examples/h2c_proxy.rs
@@ -1,3 +1,4 @@
+//! a proxy app for test
 //! test h2c proxy ,include grpc
 
 use std::{sync::Arc, time::Duration};
@@ -11,7 +12,17 @@ use pingora_core::{prelude::{background_service, HttpPeer, Opt}, server::Server,
 use pingora_load_balancing::{health_check, prelude::RoundRobin, LoadBalancer};
 use pingora_proxy::{ProxyHttp, Session};
 
-pub struct LB(Arc<LoadBalancer<RoundRobin>>);
+struct LB {
+    cli: Cli,
+    lb:Arc<LoadBalancer<RoundRobin>>
+}
+impl LB {
+    pub fn new(cli:Cli,lb:Arc<LoadBalancer<RoundRobin>>) ->Self{
+        return Self {
+            cli,lb,
+        };
+    }
+}
 #[async_trait]
 impl ProxyHttp for LB {
     type CTX = ();
@@ -19,7 +30,7 @@ impl ProxyHttp for LB {
 
     async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
         let upstream = self
-            .0
+            .lb
             .select(b"", 256) // hash doesn't matter
             .unwrap();
 
@@ -27,7 +38,11 @@ impl ProxyHttp for LB {
 
         // set upstream protocl, otherwise it will use h1 and error while proxy to h2c service
         let mut peer = HttpPeer::new(upstream, false, "127.0.0.1".to_string());
-        peer.options.alpn = ALPN::H2;
+
+        if self.cli.h2 {
+            peer.options.alpn = ALPN::H2;
+        }
+        
         let peer = Box::new(peer);
         Ok(peer)
     }
@@ -37,15 +52,17 @@ impl ProxyHttp for LB {
 pub fn main() {
     env_logger::init();
 
+    let cli = Cli::parse();
+    dbg!(&cli);
     // read command line arguments
-    let opt = Opt::parse();
-    let mut my_server = Server::new(Some(opt)).unwrap();
+    // let opt = Opt::parse();
+    let mut my_server = Server::new(Some(Opt::default())).unwrap();
     my_server.bootstrap();
 
     // only proxy to default tonic server port
-    let upstream_addr = "127.0.0.1:50051";
+    let upstream_addr = cli.upstream.as_str(); //"127.0.0.1:50051";
     info!("proxy to : {}", upstream_addr);
-    let mut upstreams = LoadBalancer::try_from_iter(["127.0.0.1:50051"]).unwrap();
+    let mut upstreams = LoadBalancer::try_from_iter([upstream_addr]).unwrap();
     let hc = health_check::TcpHealthCheck::new();
     upstreams.set_health_check(hc);
     upstreams.health_check_frequency = Some(Duration::from_secs(1));
@@ -53,12 +70,17 @@ pub fn main() {
     let background = background_service("health check", upstreams);
     let upstreams = background.task();
 
-    let listen = "0.0.0.0:6189";
-    let mut lb = pingora_proxy::http_proxy_service(&my_server.configuration, LB(upstreams));
+    let listen = cli.listen.as_str(); //"0.0.0.0:6189";
+    let mut lb = pingora_proxy::http_proxy_service(&my_server.configuration, LB::new(cli.clone(),upstreams));
     lb.add_tcp(listen);
     if let Some(http_logic) = lb.app_logic_mut() {
         let mut http_server_options = HttpServerOptions::default();
-        http_server_options.h2c = true;
+        if cli.h2 {
+            http_server_options.h2c = true;
+        }else {
+            http_server_options.h2c = false;
+        }
+        
         http_logic.server_options = Some(http_server_options);
     }
     info!("listen: {}", listen);
@@ -66,4 +88,18 @@ pub fn main() {
     my_server.add_service(lb);
     my_server.add_service(background);
     my_server.run_forever();
+}
+
+
+#[derive(Parser,Clone,Debug)]
+struct Cli {
+    /// upstream addr, for example: 127.0.0.1:50051
+    #[clap(short, long)]
+    pub upstream: String,
+    /// listen addr, for example: 0.0.0.0:6189
+    #[clap(short, long)]
+    pub listen: String,
+    /// if upstream h2,default false
+    #[clap(short, long, action)]
+    pub h2:bool,
 }
